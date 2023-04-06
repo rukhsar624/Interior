54fc2b0e7611b660fcac8ddc4d6ae1981f967edb
rukhsar624 <rukhsar56@gmail.com>
2023-04-06 22:01:42 +0500

    add

--- assets/js/main.js
+++ assets/js/main.js
@@ -302,6 +302,17 @@ var swiper = new Swiper(".mySwiper", {
       
       });
       // 
+      // eye 
+$(".toggle-password").click(function() {
+
+  $(this).toggleClass("fa-eye fa-eye-slash");
+  var input = $($(this).attr("toggle"));
+  if (input.attr("type") == "password") {
+    input.attr("type", "text");
+  } else {
+    input.attr("type", "password");
+  }
+});
   
 
 
